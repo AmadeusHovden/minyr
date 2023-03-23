1712748a33af2c515553d0ed929373e215361acb
AmadeusHovden <amadeusmh@uia.no>
2023-03-23 15:36:15 +0100

    ssss

--- main.go
+++ main.go
@@ -10,33 +10,43 @@ import (
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-
+	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Velg convert, average, eller exit: ")
-		text, _ := reader.ReadString('\n')
-		text = strings.TrimSpace(text)
+		scanner.Scan()
+		text := strings.TrimSpace(scanner.Text())
 
 		if text == "convert" {
+			_, err := os.Stat("KONV.csv")
+			if err == nil {
+				fmt.Print("Filen eksisterer allerede. Vil du generere filen på nytt? (j/n): ")
+				scanner.Scan()
+				answer := strings.ToLower(scanner.Text())
+				if answer != "j" && answer != "n" {
+					log.Fatal("Ugyldig svar")
+				} else if answer == "n" {
+					return
+				}
+			}
+
 			convertedTemperatures, err := yr.KonverterGrader()
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			err = yr.SkrivLinjer(convertedTemperatures, "KONVERTERT kjevik.txt")
+			err = yr.SkrivLinjer(convertedTemperatures, "KONV.csv")
 			if err != nil {
 				log.Fatal(err)
 			}
 
 			fmt.Println("Konvertering fullført!")
-
 		} else if text == "average" {
 			average, err := yr.GjsnittTemp()
 			if err != nil {
 				log.Fatal(err)
 			}
-			fmt.Printf("Gjennomsnittstemperaturen er: %.2f grader Celsius\n", average)
 
+			fmt.Printf("Gjennomsnittstemperaturen er: %.2f grader Celsius\n", average)
 		} else if text == "exit" {
 			break
 		} else {
